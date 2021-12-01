76575c6a836a5756556d2cbb37b179077f795077
Mahesh Adhikari <62585493+mahesh-gfx@users.noreply.github.com>
2021-12-01 22:23:23 +0530

    Added counter

--- react-redux-practice/src/main.tsx
+++ react-redux-practice/src/main.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import allReducers from "./reducers";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+let myStore = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={myStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
